45071ad82381f35b17ac9bcbe7ad95d15b21f409
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-23 15:23:25 -0700

    add targeting for 'found-it' id, rename 'find-it' from id to class, remove console log, consildate spacing.

--- js/scripts.js
+++ js/scripts.js
@@ -6,35 +6,31 @@ $(document).ready(function() {
     const answer3 = parseInt($("input:radio[name=q3]:checked").val()); // Animal
     const answer4 = parseInt($("input:radio[name=q4]:checked").val()); // Time
     const answer5 = parseInt($("input:radio[name=q5]:checked").val()); // Holiday
+
     let totalSum = parseInt(answer1 + answer2 + answer3 + answer4 + answer5)
 
     if (isNaN(totalSum)) {
       alert("You need to answer all the questions!")
     }
     else {
-      $("#find-it").hide();
-      $("#found-it").show();
+      $(".find-it").hide();
       $("#questionaire").hide();
       $(".output div").hide();
+      $("#found-it").show();
+
       if (answer5 === 3) {
         $("#ruby").show();
-      }
-      else if (answer3 === 2) {
+      } else if (answer3 === 2) {
         $("#python").show();
-      }
-      else if (answer1 === 2) {
+      } else if (answer1 === 2) {
         $("#rust").show();
-      }
-      else if (totalSum <= 11 && totalSum >=9 ) {
+      } else if (totalSum <= 11 && totalSum >=9 ) {
         $("#javascript").show();
-      }
-      else if (totalSum > 11) {
+      } else if (totalSum > 11) {
         $("#ruby").show();
-      }
-      else {
+      } else {
         $("#python").show();
       }
     }
-    console.log(totalSum);
   });
 });
