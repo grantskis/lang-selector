2c83390992ee73c74f7809034ec53961fb7f8c94
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-10-23 10:07:02 -0700

    add and nest if..else statements to guarantee successful quiz-taking.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,24 @@ $(document).ready(function() {
     const answer3 = parseInt($("input:radio[name=q3]:checked").val());
     const answer4 = parseInt($("input:radio[name=q4]:checked").val());
     const answer5 = parseInt($("input:radio[name=q5]:checked").val());
-    $("form").hide();
     let totalSum = parseInt(answer1 + answer2 + answer3 + answer4 + answer5)
-    console.log(totalSum);
     
     if (isNaN(totalSum)) {
       alert("You need to answer all the questions!")
-      $("form").show();
     }
-    else if (totalSum === 5) {
-      $("#exactly-five").show();
+    else {
+      $("#questionaire").hide();
+      $(".output span").hide();
+      $(".output #redo").show();
+      if (totalSum < 10 ) {
+        $("#low-end").show();
+      }
+      else if (totalSum === 10 ) {
+        $("#middle-end").show();
+      }
+      else if (totalSum > 10) {
+        $("#high-end").show();
+      }
     }
   });
 });
