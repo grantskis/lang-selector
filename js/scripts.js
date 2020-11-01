8969024d1a6ca2a0063725a905f3c37793c6ad3d
Shannon Grantski <72650879+grantskis@users.noreply.github.com>
2020-11-01 12:57:22 -0800

    separate business logic from UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,36 @@
+function onSubmission() {
+  const answer1 = parseInt($("input:radio[name=q1-monster]:checked").val());
+  const answer2 = parseInt($("input:radio[name=q2-zodiac]:checked").val()); 
+  const answer3 = parseInt($("input:radio[name=q3-animal]:checked").val()); 
+  const answer4 = parseInt($("input:radio[name=q4-time]:checked").val()); 
+  const answer5 = parseInt($("input:radio[name=q5-holiday]:checked").val());
+
+  let totalSum = parseInt(answer1 + answer2 + answer3 + answer4 + answer5)
+  if (isNaN(totalSum)) {
+    alert("You need to answer all the questions!")
+  } else {
+    $(".find-it").hide();
+    $("#questionaire").hide();
+    $(".output div").hide();
+    $("#found-it").show();
+
+    if (answer5 === 3) {
+      $("#ruby").show();
+    } else if (answer3 === 2) {
+      $("#python").show();
+    } else if (totalSum <= 11 && totalSum >=8 ) {
+      $("#javascript").show();
+    } else if (totalSum > 11) {
+      $("#ruby").show();
+    } else {
+      $("#python").show();
+    }
+  }
+};
+
 $(document).ready(function() {
   $("#questionaire").submit(function(event) {
     event.preventDefault();
-    const answer1 = parseInt($("input:radio[name=q1-monster]:checked").val());
-    const answer2 = parseInt($("input:radio[name=q2-zodiac]:checked").val()); 
-    const answer3 = parseInt($("input:radio[name=q3-animal]:checked").val()); 
-    const answer4 = parseInt($("input:radio[name=q4-time]:checked").val()); 
-    const answer5 = parseInt($("input:radio[name=q5-holiday]:checked").val());
-
-    let totalSum = parseInt(answer1 + answer2 + answer3 + answer4 + answer5)
-
-    if (isNaN(totalSum)) {
-      alert("You need to answer all the questions!")
-    }
-    else {
-      $(".find-it").hide();
-      $("#questionaire").hide();
-      $(".output div").hide();
-      $("#found-it").show();
-
-      if (answer5 === 3) {
-        $("#ruby").show();
-      } else if (answer3 === 2) {
-        $("#python").show();
-      } else if (totalSum <= 11 && totalSum >=8 ) {
-        $("#javascript").show();
-      } else if (totalSum > 11) {
-        $("#ruby").show();
-      } else {
-        $("#python").show();
-      }
-    }
+    onSubmission();
   });
 });
